33e6f84813ae7b2260852ac3dd8d7cea229be862
AbhayTiwari09 <88632255+AbhayTiwari09@users.noreply.github.com>
2022-03-28 23:30:33 +0530

    Add files via upload

---
+++ src/main/java/controllers/MvcConfig.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("controllers")
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	
+	
+}
